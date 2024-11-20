bafc344f10671b1716cb33f1364bab897424a2d9
Shivam Patel <shivam.p36181@gmail.com>
2024-11-20 00:45:09 -0500

    initial commit

---
+++ conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'triage.ai'
+copyright = '2024, Shivam Patel'
+author = 'Shivam Patel'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_logo = '_static/logo.svg'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False
+}
+
+html_context = {
+  'display_github': True,
+  'github_user': 'shivamrpatel',
+  'github_repo': 'docs',
+  'github_version': 'master/docs/',
+}
+
---
+++ index.rst
@@ -0,0 +1,17 @@
+.. triage.ai documentation master file, created by
+   sphinx-quickstart on Wed Nov 20 00:11:45 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+triage.ai documentation
+=======================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
